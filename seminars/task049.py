b29fc6b21029cafa943ad88b03b97fbc74775b0c
Nikita Sharov <lliapik@gmail.com>
2023-11-20 11:59:58 +0300

    max(spis, key = lambda len: len[0] * len[1]

--- seminars/task049.py
+++ seminars/task049.py
@@ -6,8 +6,9 @@ print(*find_farthest_orbit(orbits))
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
 
 def find_farthest_orbit(spis):
-    result = max(spis, key = lambda len: len[0] * len[1])
+    result = max(spis, key = lambda len: len[0] * len[1]*(len[0] != len[1]))
     # key это функция, возвращающающая ОДНО значение len для max из spis на каждой итерации spis
+    # len[0] != len[1] исключает круглые орбиты
     return result
 
 print(*find_farthest_orbit(orbits))
